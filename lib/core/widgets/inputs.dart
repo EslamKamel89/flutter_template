99246e8776c1e8c6e186dbf05f13ccb611055a30
EslamKamel89 <elsamkamelforex@gmail.com>
2025-03-11 14:29:56 +0200

    move DropDownWidget implementation from dropdown_widget.dart to inputs.dart

--- lib/core/widgets/inputs.dart
+++ lib/core/widgets/inputs.dart
@@ -495,3 +495,47 @@ class CustomTextField extends StatelessWidget {
     );
   }
 }
+
+class DropDownWidget extends StatefulWidget {
+  const DropDownWidget({super.key, required this.options, required this.label, required this.onSelect});
+  final List<String> options;
+  final String label;
+  final Function onSelect;
+  @override
+  State<DropDownWidget> createState() => _DropDownWidgetState();
+}
+
+class _DropDownWidgetState extends State<DropDownWidget> {
+  String? selectedValue;
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonFormField(
+      decoration: _decoration(widget.label),
+      items: widget.options
+          .map((option) => DropdownMenuItem<String>(
+                value: option,
+                child: txt(option),
+              ))
+          .toList(),
+      onChanged: (String? value) {
+        widget.onSelect(value);
+        setState(() {
+          selectedValue = value;
+        });
+      },
+      validator: (value) => value == null ? 'Please Select A Value' : null,
+    );
+  }
+
+  InputDecoration _decoration(String label) {
+    return InputDecoration(
+      labelText: label,
+      labelStyle: context.inputDecorationTheme.labelStyle,
+      hintStyle: context.inputDecorationTheme.hintStyle,
+      border: context.inputDecorationTheme.border,
+      enabledBorder: context.inputDecorationTheme.enabledBorder,
+      focusedBorder: context.inputDecorationTheme.focusedBorder,
+      contentPadding: context.inputDecorationTheme.contentPadding,
+    );
+  }
+}
